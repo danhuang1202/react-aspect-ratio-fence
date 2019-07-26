8e87716beb33ca803933086ccc386ace0b687423
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-26 08:00:38 +0000

    Deploy danhuang1202/react-aspect-ratio-fence to github.com/danhuang1202/react-aspect-ratio-fence.git:gh-pages

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import AspectRatioFence from './components/AspectRatioFence'
-import AspectRatioFenceLegacy from './components/AspectRatioFenceLegacy'
-import AspectRatioImg from './components/AspectRatioImg'
-import SquareFence from './components/SquareFence'
-import SquareImg from './components/SquareImg'
-
-export { AspectRatioFence, AspectRatioFenceLegacy, AspectRatioImg, SquareFence, SquareImg }
